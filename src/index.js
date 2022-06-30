2e1614063f468ac552840aaea403e9ea5425d786
Yauheniya Tarasenka <yauheniya_tarasenka@epam.com>
2022-06-30 10:54:28 +0300

    feat: code the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    num = Math.abs(n);
+    numString = num.toString();
+    numReversed = numString.split("").reverse().join("");
+    return +numReversed;
 }
